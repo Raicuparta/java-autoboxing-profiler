a7a3ccbcf3148d8ec2e42fd44b40d45a2a099b33
Raicuparta <raicuparta@gmail.com>
2016-03-24 18:42:12 +0000

    tirei toy

--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -2,9 +2,7 @@ package ist.meic.pa;
 import javassist.*;
 import javassist.expr.*;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.TreeMap;
@@ -49,7 +47,7 @@ public class BoxingProfiler {
 		if (!isWrapper(className)) return false;
 
 		for (int i = 0; i < primitives.length; i++) {
-			if (methodName.equals(primitives[i] + "Value")) {
+			if (methodName.equals(primitives[i] + "Value") && className.equals("java.lang." + wrappers[i])) {
 				return true;
 			}
 		}
